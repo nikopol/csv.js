4f33adaeb79d75a768a8b0bfa63b51e4f75d48dd
Niko <nicolas.monville@oscaro.com>
2018-11-06 15:17:40 +0100

    tidy

--- csv.js
+++ csv.js
@@ -2,49 +2,6 @@
 //niko 2018 - https://github.com/nikopol/csv.js
 
 /*
-# CSV
-
-minimalist CSV read/writer in javascript without dependancies.
-
-## reference
-
-### `CSV.read(raw[,sep[,eol]])`
-
-| parameter | default value | purpose                           |
-|-----------|---------------|-----------------------------------|
-| `raw`     |               | a string with the raw encoded csv |
-| `sep`     | ;             | columns separator                 |
-| `eol`     | \n            | rows separator                    |
-
-return: rows array of columns arrays
-
-
-### `CSV.readObjects(raw[,sep[,eol[,columns]]])`
-
-| parameter | default value    | purpose                           |
-|-----------|------------------|-----------------------------------|
-| `raw`     |                  | a string with the raw encoded csv |
-| `sep`     | ;                | columns separator                 |
-| `eol`     | \n               | rows separator                    |
-| `columns` | null (first row) | define columns names              |
-
-return: rows array of columns objects
-
-
-### `CSV.write(rows[,sep[,eol]])`
-
-| parameter | default value    | purpose                             |
-|-----------|------------------|-------------------------------------|
-| `rows`    |                  | array of arrays, objects, or scalar |
-| `sep`     | ;                | columns separator                   |
-| `eol`     | \n               | rows separator                      |
-
-return: string of the encoded csv
-
-
-## sample usage
-
-```javascript
 //read as array of arrays
 CSV.read("a;b\n1;2\n11;22")                      //=> [["a","b"],[1,2],[11,22]]
 
@@ -62,10 +19,7 @@ CSV.write([{a:1,b:2},{a:11,b:22}])               //=> "a;b\n1;2\n11;22\n"
 
 //write as array of scalars
 CSV.write([1,2,3])                               //=> "1\n2\n3\n"
-```
 */
-
-
 
 const CSV = (() => {
   "use strict";
@@ -74,12 +28,15 @@ const CSV = (() => {
     return typeof(v) === 'number' || (typeof(v) === 'string' && /^\d+(\.\d+)?$/.test(v));
   }
 
+  //decode a value
   function decode(v){
     const len = typeof(v) === 'string' ? v.length : 0;
     if( len ){
       if( v[0] === '"' && v[len-1] === '"' )
+        //remove quotes of "quoted" value & unescape quotes
         v = v.substr(1,len-2).replace(/""/g,'"');
       else {
+        //trim & unescape quotes
         v = v.replace(/^\s+|\s+$/g,'').replace(/""/g,'"');
         if( isNum(v) ) v = parseFloat(v);
       }
@@ -87,6 +44,7 @@ const CSV = (() => {
     return v;
   }
 
+  //create an encode (value) function
   function encodeFn(sep, eol){
     return v => {
       let s = '';
@@ -100,16 +58,17 @@ const CSV = (() => {
     }
   };
 
-  function uqfind(raw, pos, end, f){
+  //find next "not-in-quotes" character
+  function uqfind(raw, pos, end, char){
     let quoted = false;
     while( pos<end ){
       const c = raw[pos];
       if( c === '"' ) {
         if( pos+1<end && raw[pos+1] === '"' )
-          pos++;
+          pos++; //skip double quotes
         else
           quoted = !quoted;
-      } else if ( !quoted && c === f )
+      } else if ( !quoted && c === char )
         return pos;
       pos++;
     }
@@ -120,9 +79,11 @@ const CSV = (() => {
     let pos = 0, rows = [];
     if( raw ){
       const end = raw.length;
+      //rows loop
       while( pos<end ) {
         const row = [],
               rowend = uqfind(raw, pos, end, eol) || end;
+        //columns loop
         while( pos<rowend ) {
           const colend = uqfind(raw, pos, rowend, sep) || rowend;
           if( colend>pos ) row.push(decode(raw.substr(pos, colend-pos)));
